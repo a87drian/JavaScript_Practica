872b82ab0c6aaa4492c127c3d5ba8f0dcf15652a
Adrian Chaves Ghilardi <adrian.chaves@gmail.com>
2021-02-21 21:08:18 -0300

    Ciclos y Funciones

--- Clase3+CHAVES/js/main.js
+++ Clase3+CHAVES/js/main.js
@@ -6,7 +6,11 @@
 
 let dias = ["Lunes","Martes","Miercoles","Jueves","Viernes", "Sábado", "Domingo"];
 diasPares();
+alertDia7();
 
+let precio = ingresar();
+let precioIva = calcularIva(precio);
+mostrar(precioIva);
 
 function diasPares() {
     for(i=0; i <= dias.length; i ++) {
@@ -14,4 +18,26 @@ function diasPares() {
             alert(" " + dias[i] + " es un dia Par" )
         }
     }
+}
+function alertDia7(){
+    let i=0;
+    while(dias.length <= i){
+        
+        i++;
+        if(i + 1 == 7) {
+            alert("Descanso");
+        }
+    }
+
+}
+function ingresar() {
+    let precio = prompt("Ingrese su precio");
+    return parseFloat(precio) ;
+}
+function calcularIva(precio) {
+    precio = precio + ((precio * 21)/100 )
+    return precio;
+}
+function mostrar(precioIva) {
+    alert("El precio con Iva es " +precioIva )
 }
